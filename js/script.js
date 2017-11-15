0fbafc5a5ceab1f5861efffc5f165d818519577c
Unknown <realafe21@gmail.com>
2017-11-15 23:21:46 +0100

    some ui work here and there

--- js/script.js
+++ js/script.js
@@ -1,68 +1,104 @@
 
 $(window).on('load', function () {
-    let labels = ['weeks', 'days', 'hrs', 'minutes', 'seconds']
-        , nextYear = '2017/12/25'
-        , template = _.template($('#main-example-template').html())
-        , currDate = '00:00:00:00:00'
-        , nextDate = '00:00:00:00:00'
-        , parser = /([0-9]{2})/gi
-        , $example = $('#main-example');
+    console.clear();
 
-    // Parse countdown string to an object
-    function strfobj(str) {
-        let parsed = str.match(parser)
-            , obj = {};
-        labels.forEach(function (label, i) {
-            obj[label] = parsed[i]
-        });
-        return obj;
-    }
-    // Return the time components that diffs
-    function diff(obj1, obj2) {
-        let diff = [];
-        labels.forEach(function (key) {
-            if (obj1[key] !== obj2[key]) {
-                diff.push(key);
+    function CountdownTracker(label, value){
+
+        var el = document.createElement('span');
+
+        el.className = 'flip-clock__piece';
+        el.innerHTML = '<b class="flip-clock__card card"><b class="card__top"></b><b class="card__bottom"></b><b class="card__back"><b class="card__bottom"></b></b></b>' +
+            '<span class="flip-clock__slot">' + label + '</span>';
+
+        this.el = el;
+
+        var top = el.querySelector('.card__top'),
+            bottom = el.querySelector('.card__bottom'),
+            back = el.querySelector('.card__back'),
+            backBottom = el.querySelector('.card__back .card__bottom');
+
+        this.update = function(val){
+            val = ( '0' + val ).slice(-2);
+            if ( val !== this.currentValue ) {
+
+                if ( this.currentValue >= 0 ) {
+                    back.setAttribute('data-value', this.currentValue);
+                    bottom.setAttribute('data-value', this.currentValue);
+                }
+                this.currentValue = val;
+                top.innerText = this.currentValue;
+                backBottom.setAttribute('data-value', this.currentValue);
+
+                this.el.classList.remove('flip');
+                void this.el.offsetWidth;
+                this.el.classList.add('flip');
             }
-        });
-        return diff;
-    }
-    // Build the layout
-    let initData = strfobj(currDate);
-    labels.forEach(function (label, i) {
-        $example.append(template({
-            curr: initData[label]
-            , next: initData[label]
-            , label: label
-        }));
-    });
-    // Starts the countdown
-    $example.countdown(nextYear, function (event) {
-        var newDate = event.strftime('%w:%d:%H:%M:%S')
-            , data;
-        if (newDate !== nextDate) {
-            currDate = nextDate;
-            nextDate = newDate;
-            // Setup the data
-            data = {
-                'curr': strfobj(currDate)
-                , 'next': strfobj(nextDate)
-            };
-            // Apply the new values to each node that changed
-            diff(data.curr, data.next).forEach(function (label) {
-                var selector = '.%s'.replace(/%s/, label)
-                    , $node = $example.find(selector);
-                // Update the node
-                $node.removeClass('flip');
-                $node.find('.curr').text(data.curr[label]);
-                $node.find('.next').text(data.next[label]);
-                // Wait for a repaint to then flip
-                _.delay(function ($node) {
-                    $node.addClass('flip');
-                }, 50, $node);
-            });
         }
-    });
+
+        this.update(value);
+    }
+
+    function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        return {
+            'Total': t,
+            'days': Math.floor(t / (1000 * 60 * 60 * 24)),
+            'hrs': Math.floor((t / (1000 * 60 * 60)) % 24),
+            'min': Math.floor((t / 1000 / 60) % 60),
+            'secs': Math.floor((t / 1000) % 60)
+        };
+    }
+
+    function Clock(countdown,callback) {
+        countdown = countdown ? new Date(Date.parse(countdown)) : false;
+        callback = callback || function(){};
+
+        var updateFn = getTimeRemaining;
+
+        this.el = document.createElement('div');
+        this.el.className = 'flip-clock';
+
+        var trackers = {},
+            t = updateFn(countdown),
+            key, timeinterval;
+
+        for ( key in t ){
+            if ( key === 'Total' ) { continue; }
+            trackers[key] = new CountdownTracker(key, t[key]);
+            this.el.appendChild(trackers[key].el);
+        }
+
+        var i = 0;
+        function updateClock() {
+            timeinterval = requestAnimationFrame(updateClock);
+
+            // throttle so it's not constantly updating the time.
+            if ( i++ % 10 ) { return; }
+
+            var t = updateFn(countdown);
+            if ( t.Total < 0 ) {
+                cancelAnimationFrame(timeinterval);
+                for ( key in trackers ){
+                    trackers[key].update( 0 );
+                }
+                callback();
+                return;
+            }
+
+            for ( key in trackers ){
+                trackers[key].update( t[key] );
+            }
+        }
+
+        setTimeout(updateClock,500);
+    }
+
+//var deadline = new Date(Date.parse(new Date()) + 12 * 24 * 60 * 60 * 1000);
+    var deadline = new Date(Date.parse(new Date(`2017/12/25`)));
+    var c = new Clock(deadline, function(){ /* Do something when countdouwn is complete */ });
+    var page_timer = document.getElementById('flip_timer');
+    page_timer.appendChild(c.el);
+
     let a = [$('[bid-ctrl="b1"]').attr('count'),
         $('[bid-ctrl="b2"]').attr('count'),
         $('[bid-ctrl="b3"]').attr('count'),
@@ -75,36 +111,40 @@ $(window).on('load', function () {
         $btn.click(function(){
             timer2 = "00:00:15";
         });
+        $btn.mouseover(function () {
+           $(this).text("BIDDING STARTS DEC.1")
+        });
+        $btn.mouseleave(function () {
+           $(this).text("BID NOW")
+        });
         // console.log(timer2);
         var interval = setInterval(function() {
 
             var timer = timer2.split(':');
             //by parsing integer, I avoid all extra string processing
-            var hours = parseInt(timer[0], 10);
-            // console.log(hours);
+            var hours = (parseInt(timer[0], 10));
+            // // console.log(hours);
             var minutes = parseInt(timer[1], 10);
-            // console.log(minutes);
+            // // console.log(minutes);
             var seconds = parseInt(timer[2], 10);
-            // console.log(seconds);
+            // // console.log(seconds);
             --seconds;
-            // hours = (minutes < 0) ? --hours : hours;
-            // hours = `${(hours < 10) ? '0' + hours : hours}`;
-            // minutes = (seconds < 0) ? --minutes : minutes;
-            // minutes = (minutes < 0) ? 59 : minutes;
-            // minutes = `${(minutes < 10) ? '0' + minutes : minutes}`;
-            // seconds = (seconds < 0) ? 59 : seconds;
-            // seconds = `${(seconds < 10) ? '0' + seconds : seconds}`;
+            hours = (minutes < 0) ? --hours : hours;
+            hours = `${(hours < 10) ? '0' + hours : hours}`;
+            minutes = (seconds < 0) ? --minutes : minutes;
+            minutes = (minutes < 0) ? 59 : minutes;
+            minutes = `${(minutes < 10) ? '0' + minutes : minutes}`;
+            seconds = (seconds < 0) ? 59 : seconds;
+            seconds = `${(seconds < 10) ? '0' + seconds : seconds}`;
             if (seconds < 0) {
                 seconds = 59;
-                minutes--;
                 if (minutes < 0) {
-                    hours--;
                     minutes = 59;
                 }
             }
-            hours = `${(hours < 10) ? '0' + hours : hours}`;
-            minutes = `${(minutes < 10) ? '0' + minutes : minutes}`;
-            seconds = `${(seconds < 10) ? '0' + seconds : seconds}`;
+            // hours = `${(hours < 10) ? '0' + hours : hours}`;
+            // minutes = `${(minutes < 10) ? '0' + minutes : minutes}`;
+            // seconds = `${(seconds < 10) ? '0' + seconds : seconds}`;
 
             if (hours < 1 && minutes < 1 && seconds < 1){
                 // clearInterval(interval);
